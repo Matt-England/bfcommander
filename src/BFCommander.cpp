7ec68abbc37c3241db124143d843f98150c9f408
Matthew England <mremtf@mail.missouri.edu>
2014-02-09 17:07:16 -0600

    Added basic structure to program

---
+++ src/BFCommander.cpp
@@ -0,0 +1,18 @@
+/*
+ * BFCommander.cpp
+ *
+ *  Created on: Feb 9, 2014
+ *      Author: Matt
+ */
+#include <iostream>
+
+int main (int argc, char* argv[]) {
+	if (argc != 2) {
+		return -1;
+	}
+	std::cout << argv[1] << std::endl;
+
+}
+
+
+
